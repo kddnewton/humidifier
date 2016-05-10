737f387f1be8fb6d87a019dce5af06e8d79b8f69
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-10 13:10:59 -0400

    fix stack_exists? query for both sdks

--- test/aws_adapters/base_test.rb
+++ test/aws_adapters/base_test.rb
@@ -2,50 +2,43 @@ require 'test_helper'
 
 class BaseTest < Minitest::Test
 
-  def setup
-    @stack = Humidifier::Stack.new(name: 'test-stack')
-  end
-
   def test_deploy_stack_exists
     mock = Minitest::Mock.new
-    mock.expect(:update_stack, true, [stack_name: 'test-stack', template_body: @stack.to_cf])
+    mock.expect(:update_stack, true, [stack_name: 'test-stack', template_body: stack_double.to_cf])
 
-    with_stubbed_sdk(mock, stack_exists: true) do
-      assert @stack.deploy
-      mock.verify
+    with_sdk_v2_loaded do
+      with_stubbed_client(Humidifier::AwsAdapters::SDKV2.new, mock, stack_exists: true) do |adapter|
+        assert adapter.deploy_stack(stack_double)
+        mock.verify
+      end
     end
   end
 
   def test_deploy_stack_does_not_exist
     mock = Minitest::Mock.new
-    mock.expect(:create_stack, true, [stack_name: 'test-stack', template_body: @stack.to_cf])
+    mock.expect(:create_stack, true, [stack_name: 'test-stack', template_body: stack_double.to_cf])
 
-    with_stubbed_sdk(mock, stack_exists: false) do
-      assert @stack.deploy
-      mock.verify
+    with_sdk_v2_loaded do
+      with_stubbed_client(Humidifier::AwsAdapters::SDKV2.new, mock, stack_exists: false) do |adapter|
+        assert adapter.deploy_stack(stack_double)
+        mock.verify
+      end
+    end
+  end
+
+  def test_stack_exists?
+    assert_raises NotImplementedError do
+      Humidifier::AwsAdapters::Base.new.stack_exists?(nil)
     end
   end
 
   private
 
-  def with_stubbed_client(client)
-    Humidifier::AwsShim.instance_variable_set(:@instance, nil)
-    old_client = Humidifier::AwsShim.shim.send(:client)
-    begin
-      Humidifier::AwsShim.shim.instance_variable_set(:@client, client)
-      yield
-    ensure
-      Humidifier::AwsShim.shim.instance_variable_set(:@client, old_client)
-    end
-  end
-
-  def with_stubbed_sdk(client, stack_exists: true, &block)
-    with_faked_sdk_require do
-      with_sdk_v2_loaded do
-        AwsDouble::CloudFormation.stub(:exists?, stack_exists) do
-          with_stubbed_client(client, &block)
-        end
-      end
+  def with_stubbed_client(adapter, client, stack_exists: true)
+    AwsDouble::CloudFormation.stub(:exists?, stack_exists) do
+      adapter.instance_variable_set(:@client, client)
+      yield adapter
+      client.verify
     end
   end
 end
