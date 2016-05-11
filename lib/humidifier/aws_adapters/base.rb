158145e6a1d47719a6f482f83019fc9e196d9dae
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-11 13:46:28 -0400

    fix rubocop warning

--- lib/humidifier/aws_adapters/base.rb
+++ lib/humidifier/aws_adapters/base.rb
@@ -16,7 +16,9 @@ module Humidifier
       end
 
       def update_stack(stack, options = {})
-        try_valid { client.update_stack({ stack_name: stack.identifier, template_body: stack.to_cf }.merge(options)) }
+        try_valid do
+          client.update_stack({ stack_name: stack.identifier, template_body: stack.to_cf }.merge(options))
+        end
       end
 
       def validate_stack(stack, options = {})
