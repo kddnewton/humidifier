b4797574f80e61a09f7a9f8fdad016262f447f8d
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-24 17:29:18 -0400

    use underscored for fn

--- lib/humidifier/fn.rb
+++ lib/humidifier/fn.rb
@@ -5,7 +5,7 @@ module Humidifier
 
     # The list of all internal functions provided by AWS from
     # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference.html
-    FUNCTIONS = %w[And Base64 Equals FindInMap GetAtt GetAZs If Join Not Or Select].freeze
+    FUNCTIONS = Utils.underscored(%w[And Base64 Equals FindInMap GetAtt GetAZs If Join Not Or Select])
 
     attr_accessor :name, :value
 
@@ -20,10 +20,8 @@ module Humidifier
     end
 
     class << self
-      FUNCTIONS.each do |function|
-        define_method(Utils.underscore(function)) do |value|
-          new(function, value)
-        end
+      FUNCTIONS.each do |name, func|
+        define_method(func) { |value| new(name, value) }
       end
     end
   end
--- test/fn_test.rb
+++ test/fn_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class FnTest < Minitest::Test
 
-  Humidifier::Fn::FUNCTIONS.each do |function|
-    define_method(:"test_#{function}") do
+  Humidifier::Fn::FUNCTIONS.each do |name, func|
+    define_method(:"test_#{func}") do
       reference = Object.new
-      fn = Humidifier::Fn.send(Humidifier::Utils.underscore(function), reference)
-      assert_equal ({ "Fn::#{function}" => reference }), fn.to_cf
+      fn = Humidifier::Fn.send(func, reference)
+      assert_equal ({ "Fn::#{name}" => reference }), fn.to_cf
     end
   end
 end
