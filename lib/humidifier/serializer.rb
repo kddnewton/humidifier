03b1f54a5aaee5e9b2d653ce35b0c53c1f3f46d2
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-24 13:27:52 -0400

    add back in serializer

---
+++ lib/humidifier/serializer.rb
@@ -0,0 +1,17 @@
+module Humidifier
+
+  # Dumps an object to CFN syntax
+  class Serializer
+
+    class << self
+      def dump(node)
+        case node
+        when Hash then Utils.enumerable_to_h(node) { |(key, value)| [key, dump(value)] }
+        when Array then node.map { |value| dump(value) }
+        when Ref, Fn then dump(node.to_cf)
+        else node
+        end
+      end
+    end
+  end
+end
