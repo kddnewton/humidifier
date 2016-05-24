b1cb6f8c79b9542411f6ca1ceb54c91b48d5dc88
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-24 13:39:34 -0400

    refactor stack#to_cf to use better structure

--- lib/humidifier/stack.rb
+++ lib/humidifier/stack.rb
@@ -3,21 +3,17 @@ module Humidifier
   # Represents a CFN stack
   class Stack
 
-    STATIC_RESOURCES     = Utils.underscored(%w[AWSTemplateFormatVersion Description Metadata])
     ENUMERABLE_RESOURCES = Utils.underscored(%w[Mappings Outputs Parameters Resources])
+    STATIC_RESOURCES     = Utils.underscored(%w[AWSTemplateFormatVersion Description Metadata])
 
-    attr_accessor :id, :name, *STATIC_RESOURCES.values, *ENUMERABLE_RESOURCES.values
+    attr_accessor :id, :name, *ENUMERABLE_RESOURCES.values, *STATIC_RESOURCES.values
 
     def initialize(opts = {})
       self.name = opts[:name]
       self.id   = opts[:id]
 
-      STATIC_RESOURCES.values.each do |prop|
-        send(:"#{prop}=", opts[prop])
-      end
-      ENUMERABLE_RESOURCES.values.each do |prop|
-        send(:"#{prop}=", opts.fetch(prop, {}))
-      end
+      ENUMERABLE_RESOURCES.values.each { |prop| send(:"#{prop}=", opts.fetch(prop, {})) }
+      STATIC_RESOURCES.values.each { |prop| send(:"#{prop}=", opts[prop]) }
     end
 
     def add(name, resource)
@@ -29,22 +25,7 @@ module Humidifier
     end
 
     def to_cf
-      cf = {}
-
-      STATIC_RESOURCES.each do |name, prop|
-        resource = send(prop)
-        cf[name] = resource if resource
-      end
-
-      ENUMERABLE_RESOURCES.each do |name, prop|
-        resources = send(prop)
-        next if resources.empty?
-        cf[name] = Utils.enumerable_to_h(resources) do |resource_name, resource|
-          [resource_name, resource.to_cf]
-        end
-      end
-
-      JSON.pretty_generate(cf)
+      JSON.pretty_generate(enumerable_resources.merge(static_resources))
     end
 
     %i[mapping output parameter].each do |resource_type|
@@ -55,6 +36,25 @@ module Humidifier
 
     AwsShim::STACK_METHODS.each do |method|
       define_method(method) { |opts = {}| AwsShim.send(method, SdkPayload.new(self, opts)) }
+    end
+
+    private
+
+    def enumerable_resources
+      ENUMERABLE_RESOURCES.each_with_object({}) do |(name, prop), list|
+        resources = send(prop)
+        next if resources.empty?
+        list[name] = Utils.enumerable_to_h(resources) do |resource_name, resource|
+          [resource_name, resource.to_cf]
+        end
+      end
+    end
+
+    def static_resources
+      STATIC_RESOURCES.each_with_object({}) do |(name, prop), list|
+        resource = send(prop)
+        list[name] = resource if resource
+      end
     end
   end
 end
