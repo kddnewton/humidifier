90b8d16b33a83123bddf8ee1f359934b4419bf5e
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-24 22:02:08 -0400

    update underscore to strip out colons

--- benchmarks/underscore.rb
+++ benchmarks/underscore.rb
@@ -6,13 +6,13 @@ require 'benchmark/ips'
 
 STRINGS = Array.new(1000) { (('A'..'Z').to_a + ('0'..'9').to_a).shuffle.take(20).join }
 
-def old_underscore(str)
+def rb_underscore(str)
   str.gsub(/([A-Z]+)([0-9]|[A-Z]|\z)/) { "#{$1.capitalize}#{$2}" }.gsub(/(.)([A-Z])/, '\1_\2').downcase if str
 end
 
 Benchmark.ips do |x|
   x.report('old') do
-    STRINGS.each { |str| old_underscore(str) }
+    STRINGS.each { |str| rb_underscore(str) }
   end
   x.report('new') do
     STRINGS.each { |str| Humidifier::Utils.underscore(str) }
--- ext/humidifier/humidifier.h
+++ ext/humidifier/humidifier.h
@@ -4,8 +4,9 @@
 #include <ctype.h>
 #include <ruby.h>
 
-static void underscore_format(char* substr, const int substr_idx, const int capitalize);
-static void underscore_preprocess(char* str);
+static void filter(char* dest, const char* source);
+static void format_substring(char* substr, const int substr_idx, const int capitalize);
+static void preprocess(char* str);
 static VALUE underscore(VALUE self, VALUE str);
 
 void Init_humidifier();
