7048314cb8ae338db491d1d0aa5ec1b19dacc24f
Kevin Deisz <kevin.deisz@gmail.com>
2016-06-16 16:49:56 -0400

    drop support for ruby 2.0.0

--- test/utils_test.rb
+++ test/utils_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class UtilsTest < Minitest::Test
 
-  def test_enumerable_to_h
-    actual = Humidifier::Utils.enumerable_to_h(%w[a b c d]) { |item| [item.to_sym, item.ord] }
-    assert_equal ({ a: 97, b: 98, c: 99, d: 100 }), actual
-  end
-
   def test_underscored_empty
     assert_equal ({}), Humidifier::Utils.underscored({})
   end
