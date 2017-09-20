bf26418dddefe596aa2715119ba4b645d8e96ccf
Kevin Deisz <kevin.deisz@gmail.com>
2017-09-20 00:49:10 -0400

    Remove C extension
    While in micro benchmarks the `underscore` function compares really well against the ruby equivalent, it only actually yields about 10 milliseconds improvement on boot. Comparatively, it actually ends up pinning objects in memory that GC would otherwise be able to move, and generally makes the gem more difficult to install.

--- lib/humidifier/utils.rb
+++ lib/humidifier/utils.rb
@@ -1,6 +1,13 @@
 module Humidifier
   # Dumps an object to CFN syntax
   module Utils
+    # convert from UpperCamelCase to lower_snake_case
+    def self.underscore(name)
+      return nil unless name
+      name.gsub(/([A-Z]+)([0-9]|[A-Z]|\z)/) { "#{$1.capitalize}#{$2}" }
+          .gsub(/(.)([A-Z])/, '\1_\2').downcase
+    end
+
     # a frozen hash of the given names mapped to their underscored version
     def self.underscored(names)
       names.map { |name| [name, underscore(name).to_sym] }.to_h.freeze
